91abb7093867e96c45b8b74358083dc25f215b0e
DongHunKR <hoony77721@naver.com>
2025-05-07 07:29:37 +0100

    모터

--- pwm.py
+++ pwm.py
@@ -1,3 +1,22 @@
+'''import RPi.GPIO as GPIO
+import time
+
+LED= 10
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(LED, GPIO.OUT)
+PWM_LED= GPIO.PWM(LED, 50)
+PWM_LED.start(0)
+while True:
+	Duty_led= input('Enter Brightness (0 to 100):')
+	duty= int(Duty_led)
+	print('Duty rate: ',duty)
+	PWM_LED.ChangeDutyCycle(duty)
+PWM_LED.stop()
+print('Cleaning up!')
+GPIO.cleanup()'''
+
+
 import RPi.GPIO as GPIO
 import time
 
@@ -10,3 +29,4 @@ while True:
 	time.sleep(0.05)
 	GPIO.output(LED, GPIO.LOW)
 	time.sleep(0.01)
+
